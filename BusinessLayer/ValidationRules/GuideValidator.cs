2db662b035ad3821be272e1312923e3279c25d2c
sacilingir <seyitacilingir@gmail.com>
2025-03-26 14:25:55 +0300

    pdf doc is added

---
+++ BusinessLayer/ValidationRules/GuideValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class GuideValidator : AbstractValidator<Guide>
+    {
+        public GuideValidator()
+        {
+            RuleFor(X => X.Name).NotEmpty().WithMessage("İsim boş geçilemez.");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("İsim En az 3 harf olmalı");
+
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama boş olamaz");
+            RuleFor(x => x.Description).MinimumLength(5).WithMessage("Açıklama en az 5 harf olmalı");
+
+            RuleFor(X => X.Image).NotEmpty().WithMessage("Resim boş geçilemez.");
+        
+        }
+    }
+}
---
+++ TraversalCoreProject/Controllers/PdfReportController.cs
@@ -0,0 +1,76 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class PdfReportController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+
+        }
+
+        public IActionResult StaticPdfReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/" + "dosya1.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+
+            Paragraph paragraph = new Paragraph("Traversal Rezervasyon Pdf Raporu");
+
+            document.Add(paragraph);
+            document.Close();
+            return File("/pdfreports/dosya1.pdf", "application/pdf", "dosya1.pdf");
+            //İlk argüman: Dosya yolu 
+            //İkinci argüman: MIME türü - "application/pdf", bu, tarayıcıya dosyanın bir PDF olduğunu belirtir.
+            //Üçüncü argüman: Kullanıcıya indirilecek dosyanın adı - "dosya1.pdf", kullanıcı bu dosyayı bu ad ile indirecektir.
+        }
+
+
+
+        public IActionResult StaticCustomerReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/" + "dosya1.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+
+            PdfPTable pdfTable = new PdfPTable(3);
+
+            pdfTable.AddCell("Misafir Adı");
+            pdfTable.AddCell("Misafir Soyadı");
+            pdfTable.AddCell("Misafir TC");
+
+            pdfTable.AddCell("Nazlı");
+            pdfTable.AddCell("Cebeci");
+            pdfTable.AddCell("11111111111");
+
+            pdfTable.AddCell("Emir");
+            pdfTable.AddCell("Cebeci");
+            pdfTable.AddCell("22222222222");
+
+            pdfTable.AddCell("Zeynep");
+            pdfTable.AddCell("Cebeci");
+            pdfTable.AddCell("33333333333");
+
+            document.Add(pdfTable);
+
+            document.Close();
+
+            return File("/pdfreports/dosya2.pdf", "application/pdf", "dosya2.pdf");
+
+        }
+    }
+}
