6528a2ae9a3a9aaba864eb846d0364b0eee3d9b2
sacilingir <seyitacilingir@gmail.com>
2025-04-04 15:48:35 +0300

    exchange information was retrieved from api

---
+++ TraversalCoreProject/Areas/Admin/Controllers/ApiExchange.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    public class ApiExchange : Controller
+    {
+        [Area("Admin")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            List<BookingExchangeViewModel2> bookingExchangeViewModels = new List<BookingExchangeViewModel2>();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?locale=en-gb&currency=AED"),
+                Headers =
+    {
+        { "x-rapidapi-key", "66d9ddbefbmshb337afe43716a45p12ade0jsn7236e8fd3bcc" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingExchangeViewModel2>(body);
+                return View(values.exchange_rates);
+            }
+            
+        }
+    }
+}
---
+++ TraversalCoreProject/Areas/Admin/Controllers/ApiMovie.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiMovie : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovies = new List<ApiMovieViewModel>();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "66d9ddbefbmshb337afe43716a45p12ade0jsn7236e8fd3bcc" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovies = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovies);
+            }
+        }
+    }
+}
